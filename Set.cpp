7339c978cb6cfbe99371f1f3631c0a10f097f243
LZhizhou <luzhizhou@hotmail.com>
2019-10-01 13:49:07 -0600

    extra output

--- Set.cpp
+++ Set.cpp
@@ -1,34 +1,92 @@
 #include "Set.h"
-static const INT_BITS = sizeof(int)*8
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+static const int INT_BITS = sizeof(int) * 8;
 
 // create empty subset of {0..n-1}
 Set::Set(size_t n){
-    for (int i = 0; n > 0; i++)
-    {
-        /* code */
-    }
-    
-}
-Set::~Set();                  // destructor
-Set::Set(const Set &s);            // copy constructor
-Set &Set::operator=(const Set &s); // assignment operator
 
-void Set::clear();             // empty set
-void Set::complement();        // negate set (x was in set before <=> x is not in set after)
-bool Set::has(size_t x) const; // return true iff x is element of set (0 <= x < n)
-void Set::add(size_t x);       // add element x to set (0 <= x < n)
-void Set::remove(size_t x);    // remove element x from set (0 <= x < n)
+    Set::n = n;
+    a = ceil(n / INT_BITS);
+    bits = new unsigned int[a]();
+}
+// destructor
+Set::~Set(){
+    delete[] bits;
+}           
+// copy constructor
+Set::Set(const Set &s){
+    n = s.n;
+    a= s.a;
+    delete[] bits;
+    bits = new unsigned int[a]();
+    *bits = *(s.bits);
+}        
+// assignment operator    
+Set &Set::operator=(const Set &s){
+    if (&s!=this)
+    {
+        n = s.n;
+        a = s.a;
+        delete[] bits;
+        bits = new unsigned int[a]();
+        *bits = *(s.bits);
+    }
+    return *this;
+}
+// empty set
+void Set::clear(){
+    delete[] bits;
+    bits = new unsigned int[a]();
+} 
+// negate set (x was in set before <=> x is not in set after)
+void Set::complement(){
+    *bits = ~*bits;
+}        
+// return true iff x is element of set (0 <= x < n)
+bool Set::has(size_t x) const{
+    return (1 << x) & *bits;
+} 
+// add element x to set (0 <= x < n)
+void Set::add(size_t x){
+    *bits |= (1 << x);
+}    
+// remove element x from set (0 <= x < n)  
+void Set::remove(size_t x){
+    *bits &= ~(1 << x);
+}  
 
 // add all elements of s to set; pre-cond: s.n == n
-void Set::add(const Set &s);
+void Set::add(const Set &s){
+    assert(s.n==n);
+    *bits |= *(s.bits);
+}
 
 // remove all elements of s from set; pre-cond: s.n == n
-void Set::remove(const Set &s);
+/*
+1   1   0
+1   0   1
+0   1   0
+0   0   0
+*/
+void Set::remove(const Set &s){
+    assert(s.n==n);
+    *bits &= *bits ^ *s.bits;
+}
 
-// return maximum number of elements
-size_t Set::get_n() const { return n; }
 
 // print elements to output stream os (default std::cout) like so:
 // [ 1 2 4 5 ]
 // for set {1,2,4,5} (no extra spaces, no newline characters)
-void Set::print(std::ostream &os = std::cout) const;
+void Set::print(std::ostream &os) const{
+    os<<"[ ";
+    for (unsigned int i = 0; i < n; i++)
+    {
+        if(*bits&(1<<i)){
+            os << i << " ";
+        }
+    }
+    os << "]";
+}
